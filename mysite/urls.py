43969d2149016b5242750e2c5eea10b01c0479f7
leversonjr <38119912+leversonjr@users.noreply.github.com>
2018-05-18 21:50:34 -0300

    .

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls', include('pulls.urls'))
+    path('polls', include(('pulls.urls', 'pulls'), namespace='pulls'))
 ]
