f28dd8022f2a0c60c897557382e7a1345ba6eaae
leversonjr <38119912+leversonjr@users.noreply.github.com>
2018-05-19 11:57:19 -0300

    .

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Olá Mundo, Você está no mundo das enquetes.")
+
+def detail(request, quetion_id):
+    return HttpResponse("Você está vendo a pergunta %s" %quetion_id)
+
+def results(request, question_id):
+    response = "Você está vendo o resultado da pergunta %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando na questão %s"%question_id)
