8b09d7a81c9366a0f762c0628e346bf5c6a3326e
leversonjr <38119912+leversonjr@users.noreply.github.com>
2018-05-19 22:07:53 -0300

    .

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
 
@@ -16,8 +16,24 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "Você está vendo o resultado da pergunta %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse("Você está votando na questão %s" % question_id)
+   question = get_object_or_404(Question, pk=question_id)
+   try:
+       selected_chocie = question.choice_set.get(pk=request.POST['choice'])
+   except (KeyError, Choice.DoesNotExist):
+       # volta a exibir o formulario de votação
+        return render(request, 'polls/detail.html',{
+            'question':question,
+            'error_message': "Você não selecionou um opção"
+        })
+   else:
+       selected_chocie.votes += 1
+       selected_chocie.save()
+       # Sempre retorna um HttpResponseRedirect após lidar com sucesso
+       # com dados POST.Isso impede que os dados sejam postados duas vezes se
+       # Usuário acessa o botão Voltar.
+
+       return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
